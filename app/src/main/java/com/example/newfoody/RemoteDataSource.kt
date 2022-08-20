24a8e316b2a379c402e506e483456e7f70b0aa13
taufik0910 <taufik.cradle@gmail.com>
2022-08-21 04:52:18 +0700

    isi sintak RemoteDataSource.kt

--- app/src/main/java/com/example/newfoody/RemoteDataSource.kt
+++ app/src/main/java/com/example/newfoody/RemoteDataSource.kt
@@ -1,4 +1,14 @@
 package com.example.newfoody
 
-class RemoteDataSource {
+import com.example.newfoody.models.Antah
+import retrofit2.Response
+import java.time.temporal.TemporalQueries
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi : FoodRecipesApi
+) {
+    suspend fun getRecipes(queries: Map<String, String>): Response<Antah>{
+       return foodRecipesApi.getRecipes(queries)
+    }
 }
